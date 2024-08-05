0177a1faedb5f9293d37b88707c2da7faa9582fe
Krzysztof Brylski <krzysztof.brylski@codewave.pl>
2024-08-05 14:44:33 +0200

    #25125 added wotking dnd beetwen columns

--- src/kanban-board/components/KanbanColumn.js
+++ src/kanban-board/components/KanbanColumn.js
@@ -1,9 +1,15 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import KanbanCard from './KanbanCard';
 import { SortableContext } from '@dnd-kit/sortable';
+import { useDroppable } from '@dnd-kit/core';
 
 const KanbanColumn = ({ column, cardsArray }) => {
   const [cards, setCards] = useState(cardsArray);
+
+  const { setNodeRef } = useDroppable({
+    id: column,
+    data: { type: 'Column' },
+  });
 
   useEffect(() => {
     setCards(cardsArray);
@@ -14,8 +20,8 @@ const KanbanColumn = ({ column, cardsArray }) => {
   return (
     <div className="kanban-board__column-container" key={column}>
       <div className="kanban-board__column-header">{column}</div>
-      <div className="kanban-board__column-body">
-        <SortableContext items={cardsIds}>
+      <div className="kanban-board__column-body" ref={setNodeRef}>
+        <SortableContext items={cardsIds} id={column}>
           {cards?.map((card) => (
             <KanbanCard card={card} />
           ))}
--- src/kanban-board/components/KanbanContainer.js
+++ src/kanban-board/components/KanbanContainer.js
@@ -1,13 +1,20 @@
 import { useCallback, useEffect, useMemo, useState } from 'react';
-import { DndContext, DragOverlay } from '@dnd-kit/core';
+import {
+  DndContext,
+  DragOverlay,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
 import KanbanColumn from './KanbanColumn';
 import KanbanCard from './KanbanCard';
 import { createPortal } from 'react-dom';
+import { arrayMove, arraySwap } from '@dnd-kit/sortable';
 
 const KanbanContainer = ({ kanbanColumns, contentObjects, selectedField }) => {
   const [cards, setCards] = useState({});
 
-  const [activeCard, setActiveCard] = useState(null);
+  const [selectedCard, setSelectedCard] = useState(null);
 
   useEffect(() => {
     const cardsObj = {};
@@ -21,25 +28,141 @@ const KanbanContainer = ({ kanbanColumns, contentObjects, selectedField }) => {
     setCards(cardsObj);
   }, [kanbanColumns, contentObjects, selectedField]);
 
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 10,
+      },
+    }),
+  );
+
   const onDragStart = useCallback((event) => {
     if (event.active.data.current?.type === 'Card') {
-      setActiveCard(event.active.data.current.card);
+      setSelectedCard(event.active.data.current.card);
     }
   }, []);
 
-  useMemo(() => console.log(activeCard), [activeCard]);
+  const handleCardColumnUpdate = (
+    currentColumnId,
+    targetColumnId,
+    activeCard,
+  ) => {
+    setCards((cards) => {
+      const cardsCopy = { ...cards };
+
+      cardsCopy[currentColumnId] = cardsCopy[currentColumnId].filter(
+        (card) => card.id !== activeCard.id,
+      );
+
+      cardsCopy[targetColumnId].push({ ...activeCard });
+
+      return cardsCopy;
+    });
+  };
+
+  const handleCardOrderUpdate = (currentCard, targetCard, currentColumn) => {
+    setCards((cards) => {
+      const cardCopy = { ...cards };
+
+      const currentCardId = cardCopy[currentColumn].findIndex(
+        (card) => card.id === currentCard.id,
+      );
+
+      const targetCardId = cardCopy[currentColumn].findIndex(
+        (card) => card.id === targetCard.id,
+      );
+
+      cardCopy[currentColumn] = arrayMove(
+        cardCopy[currentColumn],
+        currentCardId,
+        targetCardId,
+      );
+
+      return cardCopy;
+    });
+  };
+
+  const onDragOver = (event) => {
+    const { active, over } = event;
+
+    if (!over || !selectedCard) return;
+
+    const activeCardId = active.id;
+    const overCardId = over.id;
+
+    if (activeCardId === overCardId) return;
+
+    const isOverCard = over.data.current?.type === 'Card';
+    const isOverColumn = over.data.current?.type === 'Column';
+
+    const activeCard = active.data.current.card;
+    const currentColumnId = active.data.current.sortable.containerId;
+
+    if (isOverCard) {
+      //handle dragging over card from other column
+      const targetColumnId = over.data.current.sortable.containerId;
+
+      if (currentColumnId === targetColumnId) return;
+
+      handleCardColumnUpdate(currentColumnId, targetColumnId, activeCard);
+    }
+
+    //handle dragging over other column
+    if (isOverColumn) {
+      const targetColumnId = over.id;
+      handleCardColumnUpdate(currentColumnId, targetColumnId, activeCard);
+    }
+  };
+
+  const onDragEnd = (event) => {
+    const { active, over } = event;
+
+    if (!over) return;
+
+    const activeCardId = active.id;
+    const overCardId = over.id;
+
+    if (activeCardId === overCardId) return;
+
+    const isOverCard = over.data.current?.type === 'Card';
+
+    if (selectedCard && isOverCard) {
+      const activeCard = active.data.current.card;
+      const overCard = over.data.current.card;
+      const currentColumn = active.data.current.sortable.containerId;
+
+      handleCardOrderUpdate(activeCard, overCard, currentColumn);
+    }
+
+    setSelectedCard(null);
+  };
+
+  const columnsContainer = useMemo(
+    () => (
+      <div className="kanban-board__container">
+        {kanbanColumns?.map((column) => (
+          <KanbanColumn column={column} cardsArray={cards[column]} />
+        ))}
+      </div>
+    ),
+    [kanbanColumns, cards],
+  );
 
   return (
     <div className="kanban-board-ui-plugin__container">
-      <DndContext onDragStart={onDragStart}>
-        <div className="kanban-board__container">
-          {kanbanColumns?.map((column) => (
-            <KanbanColumn column={column} cardsArray={cards[column]} />
-          ))}
-        </div>
-        <DragOverlay>
-          {activeCard && <KanbanCard card={activeCard} />}
-        </DragOverlay>
+      <DndContext
+        onDragStart={onDragStart}
+        onDragOver={onDragOver}
+        onDragEnd={onDragEnd}
+        sensors={sensors}
+      >
+        {columnsContainer}
+        {createPortal(
+          <DragOverlay dropAnimation={null}>
+            {selectedCard && <KanbanCard card={selectedCard} />}
+          </DragOverlay>,
+          document.body,
+        )}
       </DndContext>
     </div>
   );
