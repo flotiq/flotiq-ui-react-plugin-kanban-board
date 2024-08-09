d8430f9c5a7bb93525fb5270aee364db1845322c
Bartek Musiał <bartlomiej.musial@codewave.pl>
2024-08-09 12:48:27 +0200

    comments removed

--- src/kanban-board/components/KanbanCard.js
+++ src/kanban-board/components/KanbanCard.js
@@ -1,5 +1,5 @@
 import { useSortable } from '@dnd-kit/sortable';
-// import { CSS } from '@dnd-kit/utilities';
+import { CSS } from '@dnd-kit/utilities';
 
 const AdditionalDataRenderer = ({ data, dataKey, type }) => {
   const defaultRenderer = (dataKey, data) => (
@@ -105,7 +105,7 @@ const KanbanCard = ({
     setNodeRef,
     attributes,
     listeners,
-    // transform,
+    transform,
     transition,
     isDragging,
   } = useSortable({
